2d9d620a00584ff1a92bc653628a7a5ad86149e4
JackyLook003 <jackylook003@gmail.com>
2023-09-12 16:26:49 -0400

    refactor function => extract method: doFizzBizz'

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,33 +2,31 @@
  * Solve the FizzBuzz challenge.
  */
 class FizzBuzz {
-
     public static void main(String[] args) {
-
-        for (int i = 1; i < 100; i++) {
-
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+        int i = -100;
+        while (i < 100) {
+            i = doFizzBuzz(i);
         }
+
+    }
+
+    private static int doFizzBuzz(int i) {
+        boolean x3 = i % 3 == 0;
+        boolean x5 = i % 5 == 0;
+
+        if (x3 & x5) {
+            System.out.println("Fizz Buzz");
+        }
+        else if (x3) {
+            System.out.println("Fizz");
+        }
+        else if (x5) {
+            System.out.println("Buzz");
+        }
+        else {
+            System.out.println(i);
+        }
+        i++;
+        return i;
     }
 }
